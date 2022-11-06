85776091bcc5ed53e566831f55397ccdc6bf6674
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-06 21:53:28 +0000

    Created GetBankAccountPagedQueryHandler Unit tests

---
+++ Tuber.UnitTests/BLL/BankAccounts/Queries/GetBankAccount/GetBankccountPagedQueryHandler_UnitTests.cs
@@ -0,0 +1,97 @@
+﻿using FluentAssertions;
+using Moq;
+using Tuber.BLL.BankAccounts.Queries.GetBankAccount;
+using Tuber.BLL.Banks.Queries.GetBank;
+using Tuber.Domain.Dtos;
+using Tuber.Domain.Interfaces.Services;
+
+namespace Tuber.UnitTests.BLL.BankAccounts.Queries.GetBankAccount;
+internal class GetBankAccountPagedQueryHandler_UnitTests
+{
+    private GetBankAccountPagedQueryHandler _sut;
+    private BankAccountDto[] _bankAccountList;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _bankAccountList = new BankAccountDto[]
+        {
+            new BankAccountDto
+            {
+                 Id = Guid.NewGuid(),
+                 Name = "Bank Account 1",
+                 OrderBy = 10,
+                 IsArchived = false
+            },
+            new BankAccountDto
+            {
+                 Id = Guid.NewGuid(),
+                 Name = "Bank Account 2",
+                 OrderBy = 20,
+                 IsArchived = false
+            },
+            new BankAccountDto
+            {
+                 Id = Guid.NewGuid(),
+                 Name = "Bank Account 3",
+                 OrderBy = 30,
+                 IsArchived = false
+            },
+            new BankAccountDto
+            {
+                 Id = Guid.NewGuid(),
+                 Name = "Bank Account 4",
+                 OrderBy = 40,
+                 IsArchived = false
+            },
+            new BankAccountDto
+            {
+                 Id = Guid.NewGuid(),
+                 Name = "Deleted Bank Account",
+                 OrderBy = 99,
+                 IsArchived = true
+            }
+        };
+    }
+
+    [Test]
+    [TestCase(1, 2, 10)]
+    [TestCase(2, 2, 30)]
+    [TestCase(1, 5, 10)]
+    public void GetBankAccountPagedQueryHandler_ValidPayload_ReturnsValidResult(
+        int pageNumber, int pageSize, int orderBy)
+    {
+        var page = new ArraySegment<BankAccountDto>(_bankAccountList, (pageNumber - 1) * pageSize, pageSize).ToList();
+
+        var mockBankAccountService = new Mock<IBankAccountService>();
+
+        mockBankAccountService.Setup(x => x.GetPaged(pageNumber, pageSize))
+            .Returns(page);
+
+        var totalPages = (int)Math.Ceiling(_bankAccountList.Count(x => x.IsArchived == false) / (pageSize * 1.0));
+
+        mockBankAccountService.Setup(x => x.CountPages(pageSize))
+            .Returns(totalPages);
+
+        _sut = new GetBankAccountPagedQueryHandler(mockBankAccountService.Object);
+
+        var request = new GetBankAccountPagedQueryRequest
+        {
+            PageNumber = pageNumber,
+            PageSize = pageSize
+        };
+
+        var result = _sut.Handle(request, new CancellationToken());
+
+        //  Returned the correct number of bank rows (pageSize)
+        result.Result.BankAccountCount.Should().Be(pageSize);
+
+        //  Returned no exceptions
+        result.Result.HasExceptions.Should().BeFalse();
+
+        //  The first item has the correct OrderBy - indicating that the paging is correct.
+        result.Result.BankAccounts[0].OrderBy.Should().Be(orderBy);
+
+        result.Result.TotalPages.Should().Be(totalPages);
+    }
+}
