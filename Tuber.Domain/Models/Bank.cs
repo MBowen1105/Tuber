3945116076c8ee1b0994894a8ae90b92e44dc6c4
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-09 17:49:37 +0000

    Removed Data Annotations

--- Tuber.Domain/Models/Bank.cs
+++ Tuber.Domain/Models/Bank.cs
@@ -1,12 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Tuber.Domain.Models;
+﻿namespace Tuber.Domain.Models;
 public class Bank : EntityBase
 {
-    [Required]
-    [MaxLength(40)]
     public string? Name { get; set; }
-
-    [Range(0, int.MaxValue)]
     public int OrderBy { get; set; }
 }
--- Tuber.Domain/Models/BankAccount.cs
+++ Tuber.Domain/Models/BankAccount.cs
@@ -1,12 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Tuber.Domain.Models;
+﻿namespace Tuber.Domain.Models;
 public class BankAccount : EntityBase
 {
-    [Required]
-    [MaxLength(40)]
     public string? Name { get; set; }
-    [Range(0, int.MaxValue)]
+    
     public int OrderBy { get; set; }
     public Guid BankId { get; set; }
     public virtual Bank? Bank { get; set; }
