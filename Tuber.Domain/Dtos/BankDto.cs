a30372f4ba5b35e7c37f8b956038d1257d99f9f9
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-21 20:13:29 +0000

    Fixed BankDto mapping error

--- Tuber.Domain/Dtos/BankDto.cs
+++ Tuber.Domain/Dtos/BankDto.cs
@@ -1,10 +1,12 @@
-﻿namespace Tuber.Domain.Dtos;
+﻿using Tuber.Domain.Models;
+
+namespace Tuber.Domain.Dtos;
 public record BankDto
 {
     public Guid BankId { get; init; }
     public string Name { get; init; } = "";
     public int OrderBy { get; init; } = 0;
-    public List<BankAccountDto> BankAccounts { get; set; } = new();
+    public List<BankAccount> BankAccounts { get; set; } = new();
     public string CreatedByUserFullName { get; init; } = "";
     public DateTime CreatedOnUtc { get; init; }
     public string? UpdatedByUserFullName { get; init; }
