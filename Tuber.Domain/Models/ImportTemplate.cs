c322a87ee3b4ba2fdf24d338e51f8d4314bae6f2
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-23 17:37:46 +0000

    Fleshed Out ImportTemplate Columns

--- Tuber.Domain/Models/ImportTemplate.cs
+++ Tuber.Domain/Models/ImportTemplate.cs
@@ -5,5 +5,15 @@ public class ImportTemplate : AuditableEntity
 {
     public Guid ImportTemplateId { get; set; }
     public string TemplateName { get; set; } = "";
+    public string ImportFileName { get; set; } = "";
+    public int HeaderRows { get; set; } = 1;
+    public int? DateColumnNumber { get; set; }
+    public int? DescriptionOnStatementColumnNumber { get; set; }
+    public int? TransactionTypeColumnNumber { get; set; }
+    public int? MoneyInColumnNumber { get; set; }
+    public int? MoneyOutColumnNumber { get; set; }
+    public int? BalanceOnStatementColumnNumber { get; set; }
+    public int? SortCodeColumnNumber { get; set; }
+    public int? AccountNumberColumnNumber { get; set; }
     public bool IsDeleted { get; set; }
 }
