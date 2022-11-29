fbc877a14d641bc3a89631f07fdc1681705ce382
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-29 14:30:23 +0000

    GetImportTemplate: Added Endpoint

--- Tuber.API/ImportTemplates/ImportTemplateExtensions.cs
+++ Tuber.API/ImportTemplates/ImportTemplateExtensions.cs
@@ -2,7 +2,9 @@
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Tuber.BLL.Banks.Queries.GetBankPaged;
 using Tuber.BLL.ImportTemplates.Queries.GetImportTemplateById;
+using Tuber.Domain.Exceptions;
 
 namespace Tuber.API.ImportTemplates;
 
@@ -34,5 +36,35 @@ public static class ImportTemplateExtensions
             return Results.Ok(apiResponse);
         })
         .WithName("GetImportTemplateById");
+
+
+        app.MapGet("/importTemplate/get", async (int pageNumber, int pageSize,
+                [FromServices] IMediator mediator,
+                [FromServices] IMapper mapper) =>
+        {
+            if (pageNumber < 1)
+                return Results.BadRequest(new InvalidPageNumberException(pageNumber));
+
+            if (pageSize < 1)
+                return Results.BadRequest(new InvalidPageSizeException(pageSize));
+
+            // Call query handler. This first invokes the pipeline behaviour.
+            var queryResponse = await mediator.Send(new GetImportTemplatePagedQueryRequest
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            });
+
+            if (queryResponse.HasExceptions)
+                return Results.BadRequest(queryResponse.Exceptions);
+
+            //  Map Handler response to API Response and return.
+            var apiResponse = mapper.Map<GetImportTemplatePagedQueryResponse, GetImportTemplatePagedAPIResponse>(queryResponse);
+
+            return Results.Ok(apiResponse);
+        })
+            .WithName("GetImportTemplates");
+
+
     }
 }
