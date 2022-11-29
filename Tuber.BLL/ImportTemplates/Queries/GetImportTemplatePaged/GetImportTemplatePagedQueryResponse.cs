a4a3b4d2dfa1d08f5bf3756de9a39fc45aefc754
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-29 14:36:26 +0000

    Added: GetImportTemplatePagedQueryResponse

---
+++ Tuber.BLL/ImportTemplates/Queries/GetImportTemplatePaged/GetImportTemplatePagedQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Tuber.Core.Common;
+using Tuber.Domain.Dtos;
+
+namespace Tuber.BLL.ImportTemplates.Queries.GetImportTemplatePaged;
+public class GetImportTemplatePagedQueryResponse : PagedQueryResponseBase
+{
+    public int ImportTemplateCount { get; init; } = 0;
+    public List<ImportTemplateDto> ImportTemplates { get; init; } = new();
+}
