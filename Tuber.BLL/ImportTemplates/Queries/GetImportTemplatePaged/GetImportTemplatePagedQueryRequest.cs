46f7ddf900bf425b7c7bdfcabed565d330862eca
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-29 14:35:17 +0000

    Added PagedQueryRequest

---
+++ Tuber.BLL/ImportTemplates/Queries/GetImportTemplatePaged/GetImportTemplatePagedQueryRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Tuber.Domain.API.Common;
+
+namespace Tuber.BLL.ImportTemplates.Queries.GetImportTemplatePaged;
+public class GetImportTemplatePagedQueryRequest : PagedRequest, IRequest<GetImportTemplatePagedQueryResponse>
+{
+}
