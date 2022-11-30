38ef08141d6306c88789e910a3c4b34779282b00
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-30 15:32:17 +0000

    Added query request/response

---
+++ Tuber.BLL/BankAccounts/Queries/GetBankAccountById/GetBankAccountByIdQueryRequest.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace Tuber.BLL.BankAccounts.Queries.GetBankAccountById;
+public class GetBankAccountByIdQueryRequest : IRequest<GetBankAccountByIdQueryResponse>
+{
+    public Guid BankAccountId { get; set; }
+}
---
+++ Tuber.BLL/BankAccounts/Queries/GetBankAccountById/GetBankAccountByIdQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿using Tuber.Core.Common;
+
+namespace Tuber.BLL.BankAccounts.Queries.GetBankAccountById;
+public class GetBankAccountByIdQueryResponse : CommandQueryResponseBase
+{
+    public Guid BankAccountId { get; init; }
+    public string Name { get; init; } = "";
+    public int OrderBy { get; init; }
+}
