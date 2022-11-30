cdcc10a22f4475fa102e0836ee95c97b206f2629
Mark.Bowen <TheRealMarkBowen@gmail.com>
2022-11-30 15:41:24 +0000

    Added the API Response

---
+++ Tuber.BLL/BankAccounts/Queries/GetBankAccountById/GetBankAccountByIdAPIResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace Tuber.BLL.BankAccounts.Queries.GetBankAccountById;
+public class GetBankAccountByIdAPIResponse
+{
+    public Guid BankAccountId { get; init; }
+    public string Name { get; init; } = "";
+    public int OrderBy { get; init; }}
