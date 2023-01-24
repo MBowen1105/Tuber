5c1537233e4a26d22876ab43f3fad02ec8a509f9
Mark.Bowen <TheRealMarkBowen@gmail.com>
2023-01-24 12:12:53 +0000

    Completed LedgerAddCredit feature

--- Tuber.Application/Common/Interfaces/ILedgerUpdaterService.cs
+++ Tuber.Application/Common/Interfaces/ILedgerUpdaterService.cs
@@ -5,4 +5,10 @@ namespace Tuber.Application.Common.Interfaces;
 public interface ILedgerUpdaterService
 {
     ServiceResult<AcceptResult> Accept(Guid bankAccountId, List<Import> imports);
+    ServiceResult<Ledger> AddCredit(Guid bankAccountId, DateTime dateUtc, string description, string? reference,
+        string transactionType, double? moneyIn, Guid categoryId, Guid? subcategoryId);
+    ServiceResult<Ledger> AddDebit(Guid bankAccountId, DateTime dateUtc, string description, string? reference,
+        string transactionType, double? moneyOut, Guid categoryId, Guid? subcategoryId);
+    ServiceResult<Ledger> AddTransfer(Guid bankAccountId, DateTime dateUtc, string description, string? reference,
+        string transactionType, double? moneyOut, Guid categoryId, Guid? subcategoryId, Guid? transferBankAccountId);
 }
