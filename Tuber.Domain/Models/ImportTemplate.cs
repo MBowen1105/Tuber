de2ab1d6e94c5582c730e31af5009b68b0081a7e
Mark.Bowen <TheRealMarkBowen@gmail.com>
2023-01-26 22:42:20 +0000

    Removed Constructor for ImportTemplates

--- Tuber.Domain/Models/ImportTemplate.cs
+++ Tuber.Domain/Models/ImportTemplate.cs
@@ -5,34 +5,34 @@ public class ImportTemplate : AuditableEntity
 {
     public static readonly string FriendlyName = "Import Template";
 
-    public ImportTemplate()
-    {
-        ImportTemplateId = Guid.Empty;
-        ImportTemplateName = "";
-        ImportFileName = "";
-        ExpectedHeaderRowCount = 0;
-        ExpectedColumnCount = 0;
-        SeperatorChar = ',';
-        DateTemplate = "";
-        DateColumnNumber = 0;
-        DescriptionOnStatementColumnNumber = 0;
-        ReferenceOnStatementStartCharacter = 19;
-        TransactionTypeColumnNumber = 0;
-        MoneyInColumnNumber = 0;
-        MoneyOutColumnNumber = 0;
-        BalanceOnStatementColumnNumber = 0;
-        SortCodeColumnNumber = 0;
-        AccountNumberColumnNumber = 0;
-        IsDeleted = false;
-    }
+    //public ImportTemplate()
+    //{
+    //    ImportTemplateId = Guid.Empty;
+    //    ImportTemplateName = "";
+    //    ImportFileName = "";
+    //    ExpectedHeaderRowCount = 0;
+    //    ExpectedColumnCount = 0;
+    //    SeperatorChar = ',';
+    //    DateTemplate = "";
+    //    DateColumnNumber = 0;
+    //    DescriptionOnStatementColumnNumber = 0;
+    //    ReferenceOnStatementStartCharacter = 19;
+    //    TransactionTypeColumnNumber = 0;
+    //    MoneyInColumnNumber = 0;
+    //    MoneyOutColumnNumber = 0;
+    //    BalanceOnStatementColumnNumber = 0;
+    //    SortCodeColumnNumber = 0;
+    //    AccountNumberColumnNumber = 0;
+    //    IsDeleted = false;
+    //}
 
     public Guid ImportTemplateId { get; set; }
-    public string ImportTemplateName { get; set; }
-    public string ImportFileName { get; set; }
+    public string ImportTemplateName { get; set; } = string.Empty;
+    public string ImportFileName { get; set; } = string.Empty;
     public int ExpectedHeaderRowCount { get; set; }
     public int ExpectedColumnCount { get; set; }
     public char SeperatorChar { get; set; }
-    public string DateTemplate { get; set; }
+    public string DateTemplate { get; set; } = string.Empty;
     public int DateColumnNumber { get; set; }
     public int DescriptionOnStatementColumnNumber { get; set; }
     public int ReferenceOnStatementStartCharacter { get; set; }
